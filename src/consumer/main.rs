efd9f548c2363112fa1eef7e415763498715414a
Rob <rob.ariosa@hotmail.com>
2020-09-22 17:48:02 +0200

    Command options

--- src/consumer/main.rs
+++ src/consumer/main.rs
@@ -55,22 +55,25 @@ fn listen<'a>(
 }
 
 fn main() -> Result<()> {
-	let config = App::new("Producer program")
-		.about("Sends messages to a consumer queue")
+	let config = App::new("Consumer program")
+		.about("Processes messages from a consumer queue")
 		.arg(
 			Arg::new("url")
+				.long("url")
 				.about("URL of the service running RabbitMQ")
 				.value_name("RABBITMQ_URL")
 				.default_value("amqp://guest:guest@localhost:5672"),
 		)
 		.arg(
 			Arg::new("channel")
+				.long("channel")
 				.about("Number of the channel selected")
 				.value_name("CHANNEL_ID")
 				.default_value("1"),
 		)
 		.arg(
 			Arg::new("queue")
+				.long("queue")
 				.about("Name of the queue")
 				.value_name("QUEUE_NAME")
 				.default_value("sample"),
--- src/producer/main.rs
+++ src/producer/main.rs
@@ -32,26 +32,30 @@ fn main() -> Result<()> {
 	let config = App::new("Producer program")
 		.about("Sends messages to a consumer queue")
 		.arg(
-			Arg::new("url")
+            Arg::new("url")
+                .long("url")
 				.about("URL of the service running RabbitMQ")
 				.value_name("RABBITMQ_URL")
 				.default_value("amqp://guest:guest@localhost:5672"),
 		)
 		.arg(
-			Arg::new("channel")
+            Arg::new("channel")
+                .long("channel")
 				.about("Number of the channel selected")
 				.value_name("CHANNEL_ID")
 				.default_value("1"),
 		)
 		.arg(
-			Arg::new("queue")
+            Arg::new("queue")
+                .long("queue")
 				.about("Name of the queue")
 				.value_name("QUEUE_NAME")
 				.default_value("sample"),
 		)
 		.arg(
 			Arg::new("n-messages")
-				.short('n')
+                .short('n')
+                .long("n-messages")
 				.about("Number messages to send")
 				.value_name("N_MESSAGES")
 				.default_value("1"),
